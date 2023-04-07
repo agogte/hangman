800b9bbc1ae612ea9792a86ce6c26772352f72b4
agogte <agogte@gmu.edu>
2023-04-07 18:56:41 -0400

    added-react-components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-
+import Header from './components/Header';
+import Figure from './components/Figure';
+import WrongLetters from './components/WrongLetters';
+import Word from './components/Word';
+import Notification from './components/Notification';
+import Popup from './components/Popup';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Header />
+      <div className='game-container'>
+        <Figure />
+        <WrongLetters />
+        <Word />
+        <Notification />
+        <Popup />
+      </div>
+    </>
   );
 }
 
