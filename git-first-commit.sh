744c2188700ae549a06f4adb6e266b4201fbbbcb
agogte <agogte@gmu.edu>
2023-04-07 18:40:28 -0400

    first commit

---
+++ git-first-commit.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "repo name: ?"
+read repo_name
+git init
+git add .
+git commit -m "first commit"
+git remote add origin git@github.com:agogte/$repo_name.git
+git push -u origin master
+cowsay -T U Pushed-everything-on-github
