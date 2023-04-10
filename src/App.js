a55faf4e5ff012cb8c07c796ea18e94f34babf28
agogte <agogte@gmu.edu>
2023-04-10 13:31:15 -0400

    added-20-new-words

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { showNotification as show, checkWin } from './helpers/helpers';
 
 import './App.css';
 
-const words = ['application', 'programming', 'interface', 'wizard'];
+const words = ['flour','ghostwriter','agenda','cruelty','visual','experiment','accumulation','compromise','attention','spontaneous','countryside','atmosphere','elaborate','substitute','conviction','cigarette','disposition'];
 let selectedWord = words[Math.floor(Math.random() * words.length)];
 
 function App() {
