f5dd5fe1197517a5c102cef11458081215fde47f
agogte <agogte@gmu.edu>
2023-04-10 13:21:28 -0400

    completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,10 @@ import React from 'react'
 const Header = () => {
   return (
     <>
+      <center>
         <h1>Hangman</h1>
         <p>Find the hidden word - Enter a letter</p>
+      </center>
     </>
   )
 }
